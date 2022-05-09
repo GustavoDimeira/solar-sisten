c1a79b7b70631e72cb4982a53c85a6dbc1ff25c4
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-05-09 19:05:22 -0400

    9-10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
+    const missoes = missions.map((missao) => (<MissionCard
+      key={ missao.name }
+      name={ missao.name }
+      year={ missao.year }
+      country={ missao.country }
+      destination={ missao.destination }
+    />));
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missoes}
       </div>
     );
   }
